ed67cf94f5c75d449fa6c3baecf5fbfe17318322
G484112024 <248411@st.takushoku-u.ac.jp>
2025-05-29 21:51:36 +0900

    第6回

--- weather.js
+++ weather.js
@@ -13,32 +13,91 @@ function print(data) {
 }
 
 // 課題5-1 の関数 printDom() はここに記述すること
-function printDom(data) {
-  let s =document.querySelector('input#s');
-  console.log(s.value);
-  let u =document.querySelector('ul');
-  let i =document.querySelector('li');
+function printDom(data){
+
+
+  let d = document.querySelector('div#result');
   
 
+  d.textContent = '';
+  
+
+  let ul = document.createElement('ul');
+  
+  
+  d.insertAdjacentElement('beforeend', ul);
+  
+
+  let li1 = document.createElement('li');
+  li1.textContent = '都市名: ' + data.name;
+  ul.insertAdjacentElement('beforeend', li1);
+
+  let li2 = document.createElement('li');
+  li2.textContent = '経度: ' + data.coord.lon;
+  ul.insertAdjacentElement('beforeend', li2);
+
+  let li3 = document.createElement('li');
+  li3.textContent = '緯度: ' + data.coord.lat;
+  ul.insertAdjacentElement('beforeend', li3);
+
+  let li4 = document.createElement('li');
+  li4.textContent = '天気: ' + data.weather[0].description;
+  ul.insertAdjacentElement('beforeend', li4);
+
+  let li5 = document.createElement('li');
+  li5.textContent = '最高気温: ' + data.main.temp_max + '°C';
+  ul.insertAdjacentElement('beforeend', li5);
+
+  let li6 = document.createElement('li');
+  li6.textContent = '最低気温: ' + data.main.temp_min + '°C';
+  ul.insertAdjacentElement('beforeend', li6);
+
+  let li7 = document.createElement('li');
+  li7.textContent = '湿度: ' + data.main.humidity + '%';
+  ul.insertAdjacentElement('beforeend', li7);
+
+  let li8 = document.createElement('li');
+  li8.textContent = '風速: ' + data.wind.speed + ' m/s';
+  ul.insertAdjacentElement('beforeend', li8);
+
+  let li9 = document.createElement('li');
+  li9.textContent = '風向: ' + data.wind.deg + '°';
+  ul.insertAdjacentElement('beforeend', li9);
+
 }
 
-// 課題5-1 のイベントハンドラの定義
-function show() {
-
-}
-
-// 課題5-1, 6-1 のイベントハンドラ登録処理は以下に記述
+// 6-1 のイベントハンドラ登録処理は以下に記述
 
 
 
 
 // 課題6-1 のイベントハンドラ sendRequest() の定義
 function sendRequest() {
+  let cityId = document.querySelector('#citySelect').value;
 
-}
+      if (cityId === '') {
+        alert('都市を選択してください');
+        return;
+      }
+
+      let url = `https://www.nishita-lab.org/web-contents/jsons/openweather/${cityId}.json`;
+
+      axios.get(url)
+        .then(showResult)
+        .catch(showError)
+        .then(finish);
+    }
+
+
 
 // 課題6-1: 通信が成功した時の処理は以下に記述
 function showResult(resp) {
+  let data = resp.data;
+  if (typeof data === 'string') {
+    data = JSON.parse(data);
+  }
+  print(data);
+  printDom(data);
 
 }
 
@@ -104,5 +163,7 @@ let data = {
 };
 
 let b =document.querySelector('button#print');
-b.addEventListener('click',printDom);
+
+b.addEventListener('click', sendRequest);
+
 
